10e36e457acaf6137cffea647ee3e860fb9c344b
Juan Matheus Viana Brito <juanjd10@live.com>
2021-06-23 22:00:03 -0300

    Aula4

--- main.js
+++ main.js
@@ -10,21 +10,23 @@ for (const element of toggle) {
 const links = document.querySelectorAll('nav ul li a')
 
 for (const link of links) {
-  link.addEventListener('click', function () {
+  link.addEventListener('click', function (event) {
+    event.preventDefault()
     nav.classList.remove('show')
+    scrollSmooth(link)
   })
 }
 
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
     header.classList.remove('scroll')
   }
-})
+}
 
 /*--SWIPER--*/
 
@@ -50,7 +52,36 @@ scrollReveal.reveal(
 #about .image, #about .text,
 #services header, #services .card,
 #testimonials header, #testimonials .testimonials,
-#contact .text, #contact .links
+#contact .text, #contact .links,
+footer .brand, footer .social
 `,
   { interval: 100 }
 )
+
+/*--SCROLLSMOOTH--*/
+
+function scrollSmooth(link) {
+  const sectionId = link.getAttribute('href')
+  document.querySelector(sectionId).scrollIntoView({
+    behavior: 'smooth'
+  })
+}
+
+/*--BACK UP BUTTOM--*/
+
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/*--WHEN SCROLL--*/
+
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+})
